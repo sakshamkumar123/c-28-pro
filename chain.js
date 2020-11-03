66ca7cd743b799558945267a0a97618c1acb6a39
sakshamkumar123 <71375544+sakshamkumar123@users.noreply.github.com>
2020-11-03 15:51:40 +0530

    Update chain.js

--- chain.js
+++ chain.js
@@ -7,6 +7,7 @@ class Slingshot{
             stiffness: 0.04,
             length: 10
         }
+        this.bodyA=bodyA
         this.pointB=pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
